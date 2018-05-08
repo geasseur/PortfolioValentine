aaf302fbed97bece73f1c604d67d3edebc6f8fe5
geasseur <baptiste.ventura@neuf.fr>
2018-05-08 11:41:29 +0200

    add illustration done

--- app/Http/Controllers/IllustrationController.php
+++ app/Http/Controllers/IllustrationController.php
@@ -12,33 +12,45 @@ class IllustrationController extends Controller
 {
   public function displayIllustrations(){
      $illustrations = DB::table('illustrations')->get();
-     //dd($illustrations);
-     //affichage de la vue avec envois des variables
      return view('illustrations', compact('illustrations'));
   }
 
   public function addIllustration(Request $request){
-    //dd($_FILES['srcImage']);
-    if (request('typeImage')=='dessin') {
-      Storage::disk('illustration')->put(request('srcImage'), request('srcImage'));
+    //dd($request->srcImage->getClientOriginalName());
+    $name = $request->srcImage->getClientOriginalName();
+    $type = request('typeImage');
+    if (request('typeImage')=='illustration') {
+      Storage::disk('illustration')->put($name, request('srcImage'));
+      //récupération du nom aléatoire du fichier image pour le remplacer par le bon nom
+      $nomPif = scandir('img/illustrations/'.$type.'/'.$name.'/');
+      rename('img/illustrations/'.$type.'/'.$name.'/'.$nomPif[2], 'img/illustrations/'.$type.'/'.$name.'/'.$name);
     }
     elseif (request('typeImage')=='3D') {
-      Storage::disk('illustration3D')->put(request('srcImage'), request('srcImage'));
+      Storage::disk('illustration3D')->put($name, request('srcImage'));
+      //récupération du nom aléatoire du fichier image pour le remplacer par le bon nom
+      $nomPif = scandir('img/illustrations/'.$type.'/'.$name.'/');
+      rename('img/illustrations/'.$type.'/'.$name.'/'.$nomPif[2], 'img/illustrations/'.$type.'/'.$name.'/'.$name);
     }
-    elseif (request('typeImage')=='croquis') {
-      Storage::disk('croquis')->put(request('srcImage'), request('srcImage'));
+    elseif (request('typeImage')=='dessins') {
+      Storage::disk('dessin')->put($name, request('srcImage'));
+      //récupération du nom aléatoire du fichier image pour le remplacer par le bon nom
+      $nomPif = scandir('img/illustrations/'.$type.'/'.$name.'/');
+      rename('img/illustrations/'.$type.'/'.$name.'/'.$nomPif[2], 'img/illustrations/'.$type.'/'.$name.'/'.$name);
     }
     else {
-      Storage::disk('photo')->put(request('srcImage'), request('srcImage'));
+      Storage::disk('photo')->put($name, request('srcImage'));
+      //récupération du nom aléatoire du fichier image pour le remplacer par le bon nom
+      $nomPif = scandir('img/illustrations/'.$type.'/'.$name.'/');
+      rename('img/illustrations/'.$type.'/'.$name.'/'.$nomPif[2], 'img/illustrations/'.$type.'/'.$name.'/'.$name);
     }
     Illustration::create([
       'name'=>request('name'),
       'commentaire'=>request('commentaire'),
       'type'=>request('typeImage'),
-      'src'=>'img/'.request('typeImage').'/'.request('srcImage'),
+      'src'=>'img/illustrations/'.$type.'/'.$name.'/'.$name,
       'post'=>date('Y-m-d H-i-s'),
       'update'=>date('Y-m-d H-i-s')
     ]);
-    return redirect()->action('IllustrationController@displayIllustrations');
+    return redirect()->route('illustrations.display');
   }
 }
